84dd1f8e7f0284df8ea4041a4c10fb5041d2a3d0
AbelChanYukTo <abelchanseefat1230@gmail.com>
2025-07-26 21:21:57 +0800

    Added WS console messages

--- php.js
+++ php.js
@@ -11,7 +11,8 @@ http.createServer((req,res)=>{console.log("Received Request");
 }).listen(5500,"165.22.191.190");
 var wss=new ws.Server({port:5501,host:"165.22.191.190"});
 wss.on("connection",function (cli){console.log("WebSocket Connection Established");
-    cli.on("message",function (data){
+    cli.on("message",function (data){console.log("Received Message: "+data);
         fs.appendFile("database.txt",data,(err)=>{
             if (err){console.log("Append File Error!");}
-        })});})
+        });
+    console.log("Appended File");});});
